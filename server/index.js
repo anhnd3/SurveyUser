e454f3b68cbd51e9f8127710159cd09dd1d66b15
Nguyen Duy Anh <anhnd03@gmail.com>
2017-08-07 14:16:54 +0700

    update res test

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({ bye: 'buddy' });
+    res.send({ hi: 'buddy' });
 });
 
 const PORT = process.env.PORT || 3000;
