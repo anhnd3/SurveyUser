2416b9ffee200945b416c794397208dcefa75bdb
Nguyen Duy Anh <anhnd03@gmail.com>
2017-09-08 09:33:47 +0700

    update key deploy production

---
+++ server/config/prod.js
@@ -0,0 +1,6 @@
+module.exports = {
+    googleClientID: process.env.GOOGLE_CLIENT_ID || '367059602287-koncvtge9vclkjm18vq724abmfa10usj.apps.googleusercontent.com',
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET || 'Fh-esreVEr7NVANBU_gXFZF1',
+    mongoURI: process.env.MONGO_URI || 'mongodb://waeprod:waepassword@ds125994.mlab.com:25994/emaily-production',
+    cookieKey: process.env.COOKIE_KEY || 'waeauth'
+};
