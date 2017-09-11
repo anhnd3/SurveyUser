84a0bba4ad412aa3b93019700d35ec699a6da9e9
Nguyen Duy Anh <anhnd03@gmail.com>
2017-09-11 11:45:40 +0700

    update client reactjs

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
 
+mongoose.Promise = require('bluebird');
 mongoose.connect(keys.mongoURI);
 
 const app = express();
