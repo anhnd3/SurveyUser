d2d96dbd3a107b18f92ba08d28d7e3f7e47058ac
anhnd3 <anhnd3@vng.com.vn>
2017-10-13 15:55:11 +0700

    fix bug routing in production

--- server/index.js
+++ server/index.js
@@ -22,17 +22,18 @@ app.use(cookieSession({
 app.use(passport.initialize());
 app.use(passport.session());
 
-if(process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
-
-    const path = require('path');
-    app.get('*',(req, res)=>{
-        res.sendFile(path.resolve(__dirname, 'client', 'build','index.html'));
-    });
-}
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
+console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, function() {
